cf14933b67bfaddc4652207fb801343c011bb7d7
Maria Amigo <maria.amigo.07@gmail.com>
2024-06-20 17:25:43 +0200

    modification de maugallery js

--- assets/maugallery-full.js
+++ assets/maugallery-full.js
@@ -222,7 +222,7 @@
       var tagItems =
         '<li class="nav-item"><span class="nav-link active active-tag"  data-images-toggle="all">Tous</span></li>';
       $.each(tags, function(index, value) {
-        tagItems += `<li class="nav-item active">
+        tagItems += `<li class="nav-item">
                 <span class="nav-link"  data-images-toggle="${value}">${value}</span></li>`;
       });
       var tagsRow = `<ul class="my-4 tags-bar nav nav-pills">${tagItems}</ul>`;
